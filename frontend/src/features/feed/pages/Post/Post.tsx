0dd5623d83ef003a84ffc801367bfbfb0709fbb5
sachithn <sachithnimaliyanage@gmail.com>
2025-04-26 05:36:22 +0530

    Change feed/pages/post.tsx

--- frontend/src/features/feed/pages/Post/Post.tsx
+++ frontend/src/features/feed/pages/Post/Post.tsx
@@ -11,6 +11,7 @@ export function PostPage() {
   const { id } = useParams();
   const { user } = useAuthentication();
 
+//post
   useEffect(() => {
     request<IPost>({
       endpoint: `/api/v1/feed/posts/${id}`,
