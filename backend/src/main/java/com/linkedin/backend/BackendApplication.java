68c9f5d2d3d0a98ade7e0171ec85398e80e968b2
yasindupavithra <yasi20000422@gmail.com>
2025-04-26 09:57:20 +0530

    Update BackendApplication.java
    Test 00

--- backend/src/main/java/com/linkedin/backend/BackendApplication.java
+++ backend/src/main/java/com/linkedin/backend/BackendApplication.java
@@ -2,7 +2,7 @@ package com.linkedin.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Test 
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
