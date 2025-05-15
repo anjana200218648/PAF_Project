38e18b35210c3c8e94e610324d6b3faa40c88b6d
sachithn <sachithnimaliyanage@gmail.com>
2025-05-15 19:14:31 +0530

    do frontend changes according to backend

--- frontend/src/features/networking/components/Title/Title.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-import classes from "./Title.module.scss";
-export function Title({ children }: { children: ReactNode }) {
-  return <h2 className={classes.title}>{children}</h2>;
-}
