dd21c80bfafe839159fb94df3d2cdee563cfd2a7
yasindupavithra <yasi20000422@gmail.com>
2025-05-16 13:37:12 +0530

    Added EduFlow folder with new features

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PAF_Project
+# EduFlow
+paf
--- frontend/src/features/authentication/components/Box/Box.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-import classes from "./Box.module.scss";
-export function Box({ children }: { children: ReactNode }) {
-  return <div className={classes.root}>{children}</div>;
-}
